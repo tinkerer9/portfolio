cd0b86bd81d1a0f55a29a4e7472b280b7ddd1a39
Max Parisi <makermax101@gmail.com>
2026-07-04 17:32:09 -0700

    rename work -> projects

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,9 +3,9 @@ import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
 export const collections = {
-	work: defineCollection({
-		// Load Markdown files in the src/content/work directory.
-		loader: glob({ base: './src/content/work', pattern: '**/*.md' }),
+	projects: defineCollection({
+		// Load Markdown files in the src/content/projects directory.
+		loader: glob({ base: './src/content/projects', pattern: '**/*.md' }),
 		schema: z.object({
 			title: z.string(),
 			description: z.string(),
