cd14d0c4942d3489277c9d799974dd4bcd6a2497
Max Parisi <makermax101@gmail.com>
2026-07-06 15:50:52 -0700

    Rename Email to Contact

--- src/components/Socials.ts
+++ src/components/Socials.ts
@@ -1,6 +1,6 @@
 import type { iconPaths } from './IconPaths';
 
 export const socials: { label: string; href: string; icon: keyof typeof iconPaths }[] = [
-	{ label: 'Email', href: 'mailto:max@maxparisi.me', icon: 'paper-plane-tilt' },
+	{ label: 'Contact', href: 'mailto:max@maxparisi.me', icon: 'paper-plane-tilt' },
 	{ label: 'GitHub', href: 'https://github.com/tinkerer9', icon: 'github-logo' },
 ];
