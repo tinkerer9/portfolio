b1927d933bd2a4fa0129d01af8760c20c2d5b057
Max Parisi <makermax101@gmail.com>
2026-07-08 10:40:25 -0700

    socails update

--- src/components/Socials.ts
+++ src/components/Socials.ts
@@ -1,6 +1,5 @@
 import type { iconPaths } from './IconPaths';
 
 export const socials: { label: string; href: string; icon: keyof typeof iconPaths }[] = [
-	{ label: 'Contact', href: 'mailto:max@maxparisi.me', icon: 'paper-plane-tilt' },
 	{ label: 'GitHub', href: 'https://github.com/tinkerer9', icon: 'github-logo' },
 ];
