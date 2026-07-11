2c9d8422d4c821fdcf5a1ffdf416c1702ce6d015
Max Parisi <makermax101@gmail.com>
2026-07-10 17:41:33 -0700

    robots.txt

--- src/utils/socials.ts
+++ src/utils/socials.ts
@@ -1,5 +1,5 @@
 import type { iconPaths } from './icons';
 
 export const socials: { label: string; href: string; icon: keyof typeof iconPaths }[] = [
-	{ label: 'GitHub', href: 'https://github.com/tinkerer9', icon: 'github-logo' },
+	{ label: 'GitHub', href: 'https://go.maxparisi.me/github', icon: 'github-logo' },
 ];
