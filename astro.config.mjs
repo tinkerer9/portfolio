a3b06f38b5af3fc46dfc15b10fb9e47179040a90
Max Parisi <makermax101@gmail.com>
2026-07-10 19:47:26 -0700

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-// import cloudflare from '@astrojs/cloudflare';
-import sitemap from '@astrojs/sitemap';
-
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  // adapter: cloudflare(),
-  site: 'https://maxparisi.me',
-
-  integrations: [ sitemap() ],
-  output: 'static',
-  build: { format: 'file' },
-  trailingSlash: 'never',
-  adapter: cloudflare()
+    adapter: cloudflare(),
+    site: 'https://maxparisi.me',
+    integrations: [ sitemap() ],
+    output: 'static',
+    build: { format: 'file' },
+    trailingSlash: 'never'
 });
