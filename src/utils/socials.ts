5d900e0c642ce24c3ff40cdd5ff2262746c75478
Max Parisi <makermax101@gmail.com>
2026-07-11 18:49:28 -0700

    more redirects

--- src/utils/socials.ts
+++ src/utils/socials.ts
@@ -1,5 +1,5 @@
 import type { iconPaths } from './icons';
 
 export const socials: { label: string; href: string; icon: keyof typeof iconPaths }[] = [
-	{ label: 'GitHub', href: 'https://go.maxparisi.me/github', icon: 'github-logo' },
+	{ label: 'GitHub', href: 'https://maxparisi.me/github', icon: 'github-logo' },
 ];
