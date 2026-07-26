d6e84bfee1a22745ced7e1628ba307486ef4b6cd
Max Parisi <makermax101@gmail.com>
2026-07-26 14:26:33 -0700

    change headers for better caching

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
     adapter: cloudflare(),
     site: 'https://maxparisi.me',
