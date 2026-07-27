498977cad3d47cfbbb80e4561708273171ffef38
Max Parisi <makermax101@gmail.com>
2026-07-26 17:00:08 -0700

    use ts instead of js

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,17 +1,17 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
-export async function GET(context) {
+export async function GET(context: { site: any; }) {
     const projects = await getCollection('projects');
     return rss({
         title: "Max Parisi's Projects",
         description: "Get updated about Max's latest projects.",
         site: `${context.site}/projects`,
-        items: projects.map((entry) => ({
-            title: entry.data.title,
-            pubDate: entry.data.date,
-            description: entry.data.description,
-            link: `/projects/${entry.id}`,
+        items: projects.map((project) => ({
+            title: project.data.title,
+            pubDate: project.data.date,
+            description: project.data.description,
+            link: `/projects/${project.id}`,
         })),
     });
 }
