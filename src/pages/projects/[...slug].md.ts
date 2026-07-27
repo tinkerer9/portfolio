f5f7efaae91a240c10a733cb035707d1d4fa4a55
Max Parisi <makermax101@gmail.com>
2026-07-26 17:01:21 -0700

    rename entry -> project

--- src/pages/projects/[...slug].md.ts
+++ src/pages/projects/[...slug].md.ts
@@ -2,28 +2,28 @@ import { type APIContext } from 'astro';
 import { type CollectionEntry, getCollection } from 'astro:content';
 
 interface Props {
-	entry: CollectionEntry<'projects'>;
+	project: CollectionEntry<'projects'>;
 }
 
 export async function getStaticPaths() {
 	const projects = await getCollection('projects');
 
-	return projects.map((entry) => ({
-		params: { slug: entry.id },
-		props: { entry }
+	return projects.map((project) => ({
+		params: { slug: project.id },
+		props: { project }
 	}));
 }
 
 export async function GET({ props, url }: APIContext & { props: Props }) {
-	const { entry } = props;
+	const { project } = props;
 
-	if (!entry.body) {
+	if (!project.body) {
 		return new Response('Project has no Markdown body.', { status: 500 });
 	}
 
 	const site = url.origin;
 
-    const body = entry.body
+    const body = project.body
         // use full path for links and images:
         .replace(
             /(!?)\[([^\]]*)\]\(([^)\s]+)([^)]*)\)/g,
@@ -37,11 +37,11 @@ export async function GET({ props, url }: APIContext & { props: Props }) {
         // keep newline spacing clean:
         .replace(/\n{3,}/g, '\n\n');
     
-	const markdown = `# ${entry.data.title}
+	const markdown = `# ${project.data.title}
 
-${entry.data.description}
+${project.data.description}
 
-![${entry.data.img_alt ?? ''}](${new URL(entry.data.img, site).href})
+![${project.data.img_alt ?? ''}](${new URL(project.data.img, site).href})
 
 ${body}
 `;
