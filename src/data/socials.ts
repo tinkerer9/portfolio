f060ed11478a5be425cdcc79b14182a8ddfa4257
Max Parisi <makermax101@gmail.com>
2026-07-31 17:00:14 -0700

    add arduino profile (not on socials yet)

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,5 +1,6 @@
 import type { iconPaths } from './icons';
 
 export const socials: { label: string; href: string; icon: keyof typeof iconPaths }[] = [
-	{ label: 'GitHub', href: 'https://maxparisi.me/github', icon: 'github-logo' }
+	{ label: 'GitHub', href: 'https://maxparisi.me/github', icon: 'github-logo' },
+	// { label: 'Arduino', href: 'https://maxparisi.me/github', icon: 'arduino-logo' }
 ];
