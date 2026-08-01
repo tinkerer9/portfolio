98c2c2d0b856d45d75853ad3b65eb61a16b97561
Max Parisi <makermax101@gmail.com>
2026-07-31 17:00:23 -0700

    typo

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -2,5 +2,5 @@ import type { iconPaths } from './icons';
 
 export const socials: { label: string; href: string; icon: keyof typeof iconPaths }[] = [
 	{ label: 'GitHub', href: 'https://maxparisi.me/github', icon: 'github-logo' },
-	// { label: 'Arduino', href: 'https://maxparisi.me/github', icon: 'arduino-logo' }
+	// { label: 'Arduino', href: 'https://maxparisi.me/arduino', icon: 'arduino-logo' }
 ];
