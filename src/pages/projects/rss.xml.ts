f2e92646e938f2c165a0a9515d88c530038850ae
Max Parisi <max@maxparisi.me>
2026-08-05 08:52:21 -0700

    more rss tags

--- src/pages/projects/rss.xml.ts
+++ src/pages/projects/rss.xml.ts
@@ -12,36 +12,49 @@ function escapeXml(text: string) {
 }
 
 export async function GET() {
-	const projects = (await getCollection('projects'))
+	const projects = (await getCollection("projects"))
 		.sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf());
 
-	const lastBuildDate = projects.length
+	const lastUpdatedDate = projects.length
 		? projects.reduce(
-			(latest, project) => project.data.updated > latest ? project.data.updated : latest,
-			projects[0].data.updated
-		)
+				(latest, project) =>
+					project.data.updated > latest ? project.data.updated : latest,
+				projects[0].data.updated
+			)
 		: new Date();
 
 	const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-<channel>
+  <channel>
+    <title>Max Parisi's Projects</title>
+    <description>Get updated about Max's latest projects.</description>
+    <link>${escapeXml(`${SITE}/projects`)}</link>
+    <language>en-us</language>
     <atom:link href="${escapeXml(`${SITE}/projects/rss.xml`)}" rel="self" type="application/rss+xml" />
-	<title>Max Parisi's Projects</title>
-	<description>Get updated about Max's latest projects.</description>
-	<link>${escapeXml(`${SITE}/projects`)}</link>
-	<language>en-us</language>
-	<lastBuildDate>${lastBuildDate.toUTCString()}</lastBuildDate>
+    <image>
+      <url>${escapeXml(`${SITE}/logos/color.png`)}</url>
+      <title>Max Parisi</title>
+      <link>${escapeXml(SITE)}</link>
+    </image>
+    <lastBuildDate>${lastUpdatedDate.toUTCString()}</lastBuildDate>
+    <pubDate>${lastUpdatedDate.toUTCString()}<pubDate>
 ${projects
-		.map((project) => `	<item>
-		<title>${escapeXml(project.data.title)}</title>
-		<description>${escapeXml(project.data.description)}</description>
-		<link>${escapeXml(`${SITE}/projects/${project.id}`)}</link>
-		<guid isPermaLink="true">${escapeXml(`${SITE}/projects/${project.id}`)}</guid>
-		<pubDate>${project.data.date.toUTCString()}</pubDate>
-	</item>`).join("\n")}
-</channel>
+	.map(
+		(project) => `    <item>
+      <title>${escapeXml(project.data.title)}</title>
+      <description>${escapeXml(project.data.description)}</description>
+${project.data.tags
+	.map((tag) => `      <category>${escapeXml(tag)}</category>`)
+	.join("\n")}
+      <link>${escapeXml(`${SITE}/projects/${project.id}`)}</link>
+      <guid isPermaLink="true">${escapeXml(`${SITE}/projects/${project.id}`)}</guid>
+      <pubDate>${project.data.date.toUTCString()}</pubDate>
+    </item>`
+	)
+	.join("\n")}
+  </channel>
 </rss>
 `;
 
 	return new Response(xml);
-};
+}
