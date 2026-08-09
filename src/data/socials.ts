48daf88cb95008f0b497ac665c30abd472a0632d
Max Parisi <max@maxparisi.me>
2026-08-09 13:14:46 -0700

    use actual socials link instead of reditect

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,6 +1,6 @@
 import type { iconPaths } from './icons';
 
 export const socials: { label: string; href: string; icon: keyof typeof iconPaths }[] = [
-    { label: 'GitHub', href: 'https://maxparisi.me/github', icon: 'github-logo' },
-    // { label: 'Arduino', href: 'https://maxparisi.me/arduino', icon: 'arduino-logo' }
+    { label: 'GitHub', href: "https://github.com/tinkerer9", icon: 'github-logo' },
+    // { label: 'Arduino', href: "https://forum.arduino.cc/u/makermax101", icon: 'arduino-logo' }
 ];
