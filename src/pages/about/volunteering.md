6f21ea86cb4876eaaa2d0497e72f5098e2aa48a7
Max Parisi <max@maxparisi.me>
2026-08-09 12:04:07 -0700

    404 rework

---
+++ src/pages/about/volunteering.md
@@ -0,0 +1,8 @@
+---
+layout: ../../layouts/MarkdownLayout.astro
+title: Volunteering
+---
+
+<!-- markdownlint-disable MD001 -->
+
+**This page isn't yet completed&mdash;please come back later!**
--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,7 +1,5 @@
 import { pages } from "../data/pages";
 
-const SITE = "https://maxparisi.me";
-
 function escapeXml(text: string) {
 	return text
 		.replace(/&/g, "&amp;")
@@ -11,11 +9,13 @@ function escapeXml(text: string) {
 		.replace(/'/g, "&apos;");
 }
 
-export async function GET() {
+export async function GET(context: { site: URL }) {
+	const site = context.site.origin;
+
 	const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${pages.map((page) => `  <url>
-    <loc>${escapeXml(`${SITE}${page.path}`)}</loc>${
+    <loc>${escapeXml(`${site}${page.path}`)}</loc>${
 		page.updated ? `\n    <lastmod>${page.updated.toISOString().slice(0, 10)}</lastmod>` : ""
 	}
   </url>`).join("\n")}
