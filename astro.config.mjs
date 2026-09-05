53edec408925acd3681605401ac02bdbb493d759
Max Parisi <max@maxparisi.me>
2026-09-04 18:41:49 -0700

    add satteri plugin for external links (uses _blank)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import { satteri } from "@astrojs/markdown-satteri";
+import { externalLinks } from "./src/plugins/externalLinks";
 
 export default defineConfig({
     adapter: cloudflare(),
     site: 'https://maxparisi.me',
     trailingSlash: 'never',
-    prefetch: { prefetchAll: true }
+    prefetch: { prefetchAll: true },
+    markdown: {
+        processor: satteri({
+            hastPlugins: [ externalLinks ]
+        })
+    }
 });
